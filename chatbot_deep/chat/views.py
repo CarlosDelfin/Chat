76aa9aff996f931e147f0e7bd78cabfc9784da8c
Jose Irola <joseangel0817@gmail.com>
2025-03-16 15:21:37 -0500

    inicio del backend para la comunicacion con el bot

--- chatbot_deep/chat/views.py
+++ chatbot_deep/chat/views.py
@@ -1,3 +1,31 @@
+import requests
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+DEEPSEEK_API_URL = "https://api.deepseek.com/v1/chat/completions"
+
+DEEPSEEK_API_KEY = "sk-b0f392d19e834bbeb271b3f04e50c1aa"
+
+def chat(request):
+    if request.method == 'POST':
+        user_message = request.POST.get('message')
+
+        #llamar a la api
+        headers = {
+            'Authorization': f'Bearer {DEEPSEEK_API_KEY}',
+            'Content-Type': 'application/json',
+        }
+        data = {
+            'model': 'deepseek-v3',
+            'messages': [{'role':'user', 'content': user_message}],
+        }
+        
+        response = requests.post(DEEPSEEK_API_URL, headers = headers, json=data)
+        
+        if response.status_code == 200:
+            bot_response = response.json()["choices"][0]["message"]["content"]
+            return JsonResponse({'response': bot_response})
+        else: 
+            return JsonResponse({'error': "Error al comunicarse con la API"}, status=500)
+
+    return render(request, 'chat/index.html')    
