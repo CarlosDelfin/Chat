a82fdc39fb86e833d0f2f178028f9aa137182285
Jose Irola <joseangel0817@gmail.com>
2025-03-16 14:52:59 -0500

    reestructuracion del proyecto, inicio de las declaraciones y primeros archivos

--- chatbot_deep/chatbot_deep/urls.py
+++ chatbot_deep/chatbot_deep/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ChatBot project.
+URL configuration for chatbot_deep project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
