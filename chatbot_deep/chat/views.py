c7457bdcfb11bb78d7e08467c11291542fab0605
Jose Irola <joseangel0817@gmail.com>
2025-03-16 17:46:50 -0500

    proyecto funcional v1

--- chatbot_deep/chat/views.py
+++ chatbot_deep/chat/views.py
@@ -1,31 +1,44 @@
 import requests
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-DEEPSEEK_API_URL = "https://api.deepseek.com/v1/chat/completions"
+DEEPSEEK_API_URL = "https://api.deepseek.com/chat/completions"
 
 DEEPSEEK_API_KEY = "sk-b0f392d19e834bbeb271b3f04e50c1aa"
+
+@csrf_exempt
 
 def chat(request):
     if request.method == 'POST':
         user_message = request.POST.get('message')
-
-        #llamar a la api
-        headers = {
-            'Authorization': f'Bearer {DEEPSEEK_API_KEY}',
-            'Content-Type': 'application/json',
-        }
-        data = {
-            'model': 'deepseek-v3',
-            'messages': [{'role':'user', 'content': user_message}],
-        }
+        user_message = 'hola'
+        print('msg: ', user_message)
+        try:
+            #llamar a la api
+            header = {
+                'Authorization': f'Bearer {DEEPSEEK_API_KEY}',
+                'Content-Type': 'application/json',
+            }
+            data = {
+                'model': 'deepseek-chat',
+                'messages': [{'role':'user', 'content': user_message}],
+            }
+            
+            response = requests.post(DEEPSEEK_API_URL, headers = header, json=data)
+            response.raise_for_status()
+            
+            if response.status_code == 200:
+                bot_response = response.json()["choices"][0]["message"]["content"]
+                return JsonResponse({'response': bot_response})
+            else: 
+                return JsonResponse({'error': "Error al comunicarse con la API"}, status=response.status_code)
         
-        response = requests.post(DEEPSEEK_API_URL, headers = headers, json=data)
+        except requests.exceptions.RequestException as e:
+            # Captura errores de la solicitud a la API
+            return JsonResponse({"error": f"Error al comunicarse con la API: {str(e)}"}, status=response.status_code)
+        except KeyError as e:
+            # Captura errores si la estructura de la respuesta no es la esperada
+            return JsonResponse({"error": f"Error en la respuesta de la API: {str(e)}"}, status=response.status_code)
         
-        if response.status_code == 200:
-            bot_response = response.json()["choices"][0]["message"]["content"]
-            return JsonResponse({'response': bot_response})
-        else: 
-            return JsonResponse({'error': "Error al comunicarse con la API"}, status=500)
-
     return render(request, 'chat/index.html')    
