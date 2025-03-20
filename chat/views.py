34d32d9fa00c456fb6b3ec63931a904fd8baacae
Jose Irola <joseangel0817@gmail.com>
2025-03-20 07:56:34 -0500

    minor changes

--- chat/views.py
+++ chat/views.py
@@ -19,30 +19,33 @@ with open('chat\context.txt', mode = 'r') as file:
 
 def chat(request):
     if request.method == 'POST':
-        user_message = request.POST.get('message')
-        try:
-            #llamar a la api
-            header = {
-                'Authorization': f'Bearer {DEEPSEEK_API_KEY}',
-                'Content-Type': 'application/json',
-            }
-            data = {
-                'model': 'deepseek-chat',
-                'messages': [{'role':'system', 'content': context},
-                             {'role':'user', 'content': user_message}],
-            }
-            
-            response = requests.post(DEEPSEEK_API_URL, headers = header, json=data)
-            response.raise_for_status()
-            
-            bot_response = response.json()["choices"][0]["message"]["content"]
-            return JsonResponse({'response': bot_response})
-        
-        except requests.exceptions.RequestException as e:
-            # Captura errores de la solicitud a la API
-            return JsonResponse({"error": f"Error al comunicarse con la API: {str(e)}"}, status=response.status_code)
-        except KeyError as e:
-            # Captura errores si la estructura de la respuesta no es la esperada
-            return JsonResponse({"error": f"Error en la respuesta de la API: {str(e)}"}, status=response.status_code)
-        
+        match request.POST.get('obj'):
+            case 'api':
+                user_message = request.POST.get('message')
+                try:
+                    #llamar a la api
+                    header = {
+                        'Authorization': f'Bearer {DEEPSEEK_API_KEY}',
+                        'Content-Type': 'application/json',
+                    }
+                    data = {
+                        'model': 'deepseek-chat',
+                        'messages': [{'role':'system', 'content': context},
+                                    {'role':'user', 'content': user_message}],
+                    }
+                    
+                    response = requests.post(DEEPSEEK_API_URL, headers = header, json=data)
+                    response.raise_for_status()
+                    
+                    bot_response = response.json()["choices"][0]["message"]["content"]
+                    return JsonResponse({'response': bot_response})
+                
+                except requests.exceptions.RequestException as e:
+                    # Captura errores de la solicitud a la API
+                    return JsonResponse({"error": f"Error al comunicarse con la API: {str(e)}"}, status=response.status_code)
+                except KeyError as e:
+                    # Captura errores si la estructura de la respuesta no es la esperada
+                    return JsonResponse({"error": f"Error en la respuesta de la API: {str(e)}"}, status=response.status_code)
+            case 'speechToTxt':
+                pass    
     return render(request, 'chat/index.html')    
